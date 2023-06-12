b22862d19c263a0aa985353586c715ed5f75685c
JayavelAnandh <jvanandh984@gmail.com>
2023-06-12 14:58:52 +0530

    provided bgs

--- src/pages/CreateBlog.js
+++ src/pages/CreateBlog.js
@@ -83,88 +83,90 @@ const CreateBlog = () => {
     };
   }, []);
   return (
-    <div className="container-xl createBlogPage">
-      {loading ? (
-        <iframe
-          src="https://embed.lottiefiles.com/animation/94994"
-          style={{ height: "100%", width: "100%" }}
-        ></iframe>
-      ) : (
-        <>
-          <div className="imgDiv">
-            {pic && <img className="writeImg" src={pic} alt="" />}
-          </div>
-          <form>
-            <span>
-              <label htmlFor="pic" className="imageLabel">
-                {!pic ? (
-                  <>
-                    <i
-                      className="fa-solid fa-plus"
-                      style={{ marginRight: "3px" }}
-                    ></i>{" "}
-                    Share your memories !
-                  </>
-                ) : (
-                  <>
-                    <i
-                      className="fa-solid fa-repeat"
-                      style={{ marginRight: "8px" }}
-                    ></i>
-                    Replace the photo
-                  </>
-                )}
-              </label>
-            </span>
-            <input
-              type="file"
-              id="pic"
-              name="pic"
-              accept="image/*"
-              onChange={(event) => postDetails(event.target.files[0])}
-              required
-              style={{ display: "none" }}
-            />
-            <div className="titleInput">
-              <label htmlFor="title">Title</label>
-              <div>
-                <input
-                  id="title"
-                  name="title"
-                  type="text"
-                  placeholder="Title"
-                  className="writeInput"
-                  autoFocus={true}
-                  value={title}
-                  onChange={(event) => setTitle(event.target.value)}
-                />
-                <button
-                  className="writeSubmit btn btn-outline-info"
-                  type="submit"
-                  onClick={(e) => handleSubmit(e)}
-                >
-                  {width > 800 ? (
-                    "Publish"
-                  ) : (
-                    <i className="fa-solid fa-upload"></i>
-                  )}
-                </button>
-              </div>
+    <div className="container-fluid fullCBpage">
+      <div className="container-xl createBlogPage">
+        {loading ? (
+          <iframe
+            src="https://embed.lottiefiles.com/animation/94994"
+            style={{ height: "100%", width: "100%" }}
+          ></iframe>
+        ) : (
+          <>
+            <div className="imgDiv">
+              {pic && <img className="writeImg" src={pic} alt="" />}
             </div>
+            <form>
+              <span>
+                <label htmlFor="pic" className="imageLabel">
+                  {!pic ? (
+                    <>
+                      <i
+                        className="fa-solid fa-plus"
+                        style={{ marginRight: "3px" }}
+                      ></i>{" "}
+                      Share your memories !
+                    </>
+                  ) : (
+                    <>
+                      <i
+                        className="fa-solid fa-repeat"
+                        style={{ marginRight: "8px" }}
+                      ></i>
+                      Replace the photo
+                    </>
+                  )}
+                </label>
+              </span>
+              <input
+                type="file"
+                id="pic"
+                name="pic"
+                accept="image/*"
+                onChange={(event) => postDetails(event.target.files[0])}
+                required
+                style={{ display: "none" }}
+              />
+              <div className="titleInput">
+                <label htmlFor="title">Title</label>
+                <div>
+                  <input
+                    id="title"
+                    name="title"
+                    type="text"
+                    placeholder="Title"
+                    className="writeInput"
+                    autoFocus={true}
+                    value={title}
+                    onChange={(event) => setTitle(event.target.value)}
+                  />
+                  <button
+                    className="writeSubmit btn btn-outline-info"
+                    type="submit"
+                    onClick={(e) => handleSubmit(e)}
+                  >
+                    {width > 800 ? (
+                      "Publish"
+                    ) : (
+                      <i className="fa-solid fa-upload"></i>
+                    )}
+                  </button>
+                </div>
+              </div>
 
-            <textarea
-              placeholder="Share Your Story ..."
-              value={description}
-              className="writeInput writeText"
-              onChange={(event) => {
-                setDescription(event.target.value);
-                console.log(description);
-                console.log(typeof description);
-              }}
-            />
-          </form>
-        </>
-      )}
+              <textarea
+                placeholder="Share Your Story ..."
+                value={description}
+                className="writeInput writeText"
+                onChange={(event) => {
+                  setDescription(event.target.value);
+                  console.log(description);
+                  console.log(typeof description);
+                }}
+              />
+            </form>
+          </>
+        )}
+      </div>
     </div>
   );
 };
